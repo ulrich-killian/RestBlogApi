444eaad728a7225e97af764f07498125461f17db
79Rcf <197534687+79Rcf@users.noreply.github.com>
2026-06-03 13:21:59 +0100

    feat: controller auth setup for register login with res.status code upon success messages

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,51 @@
+import * as authService from '../services/auth.service.js';
+import { pool } from '../config/db.config.js';
+
+export const register = async (req, res) => {
+   try {
+      const { username, email, password } = req.body;
+      const result = await authService.register({ username, email, password });
+      res.status(201).json({ success: true, ...result });
+   } catch (error) {
+      if (error.message === 'Email or username already exist'){
+         return res.status(409).json({ message: 'Email or username already exist ' });
+      }
+      res.status(500).json({  message: 'Server error', error: error.message })
+   }
+};
+
+export const login = async (req, res) => {
+     try {
+      const user = await authService.getUser(req.user.id);
+      res.status(200).json({ success: true, data: user });
+     } catch (error) {
+      if (error.message === 'USER_NOT_FOUND') {
+         return res.status(404).json({ message: 'User not found' });
+     }
+     res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+
+export const uploadProfilePicture = async (req, res) => {
+   try {
+     if (!req.file) {
+       return res.status(400).json({ message: 'No file uploaded' });
+     }
+ 
+     const fileUrl = `/uploads/${req.file.filename}`;
+ 
+     await pool.query(
+       'UPDATE users SET profile_picture = $1 WHERE id = $2',
+       [fileUrl, req.user.id]
+     );
+ 
+     res.status(200).json({
+       success: true,
+       message: 'Profile picture uploaded successfully',
+       profile_picture: fileUrl
+     });
+   } catch (error) {
+     res.status(500).json({ message: 'Error uploading profile picture', error: error.message });
+   }
+ };
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
-import { pool, connectDb } from '../config/db.config.js'
+import { pool } from '../config/db.config.js'
 
 export const register  = async ({ username, password, email }) => {
       const existing = await pool.query(
