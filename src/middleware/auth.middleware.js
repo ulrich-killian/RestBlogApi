653a00840151195b63b6371b027393e3e0a18450
79Rcf <197534687+79Rcf@users.noreply.github.com>
2026-06-03 13:29:33 +0100

    added a middleware to verify if the token

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,17 @@
+import jwt, { decode } from 'jsonwebtoken'
+
+export const protectEntry = async (req, res, next) => {
+   let token = req.headers.authorization;
+   if(!token || !token.startWith('Bearer')) {
+      return res.status(401).json({ message: 'Unauthorized, missing token' })
+   }
+
+   token = token.split(' ')[1];
+   try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      req.user = decode;
+      next();
+   } catch (error) {
+      return res.status(401).json({ message: 'Unauthorized, invalid token', err });
+   }
+};
